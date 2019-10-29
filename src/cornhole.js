3a4f6e4bc2cd9cb23c1a3c8c3a7c267d3e01a112
Zoe O <zoeo@Zoes-MBP.home>
2019-10-28 23:25:42 -0400

    fixed mistakes

--- src/cornhole.js
+++ src/cornhole.js
@@ -2,10 +2,10 @@ const readlineSync = require("readline-sync");
 const length = 48;
 const width = 24;
 const diameter = 6;
-const area = 48*24;
+const areaOfBoard = 48*24;
 //area of a circle = 3.14r^2
 //r = diamater*1/2
-const area2 = (9)*3.141;
-let surfacearea = (area) - (area2);
+const areaOfCircle = (9)*3.141;
+let surfacearea = (areaOfBoard) - (areaOfCircle);
 surfacearea=surfacearea.toLocaleString("en", { minimumFractionDigits: 2, maximumFractionDigits: 2});
 console.log("\nThe surface area of a standard Cornhole board is "+ (surfacearea)+ " square inch(es).\n");
--- src/salary.js
+++ src/salary.js
@@ -2,10 +2,11 @@ const readlineSync=require("readline-sync");
 let salary = readlineSync.question("\nAnnual salary: ");
 salary = salary/24;
 const pretax = (salary*.93);
-const fed =pretax*.157;
-const inc = pretax*.0447;
-const soc = pretax*.062;
-const med = pretax*.0145;
-let total = pretax-(fed+inc+soc+med);
+const federalIncomeTax =pretax*.157;
+const stateIncomeTax = pretax*.0447;
+const socialSecurityTax = pretax*.062;
+const medicareTax = pretax*.0145;
+let total = pretax-(federalIncomeTax+stateIncomeTax+socialSecurityTax+medicareTax);
 total = total.toLocaleString("en", { minimumFractionDigits: 2, maximumFractionDigits: 2});
 console.log("\nYour take-home pay each check will be $"+ total+".\n");
+ 
